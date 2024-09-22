0c1d991f332e0d9d35b5d8c8681c3bee50417491
Sal Olivares <git@salolivares.com>
2024-09-22 01:35:52 -0700

    more stuff

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -21,6 +21,18 @@ return {
     },
   },
   {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
+  {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function(_, opts)
